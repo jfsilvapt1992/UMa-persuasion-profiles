03532f4c4860b7fe2433a2c5544d064eb2ccda68
jfsilva <jose.fabricio.pst@pst.asseco.com>
2021-01-22 00:18:18 +0000

    fixed registration form

--- src/main/java/com/uma/mestrado/persuasive_profiles/controllers/ViewLoginController.java
+++ src/main/java/com/uma/mestrado/persuasive_profiles/controllers/ViewLoginController.java
@@ -7,12 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.uma.mestrado.persuasive_profiles.services.PersonService;
-import com.uma.mestrado.persuasive_profiles.services.UtilsServices;
 
 import swaggerCodegen.models.GetLoginResponse;
 
@@ -24,13 +23,10 @@ public class ViewLoginController
   Logger logger = LoggerFactory.getLogger(ViewLoginController.class);
 
   @Autowired
-  private UtilsServices utilsService;
-  @Autowired
   private PersonService personService;
 
-  @RequestMapping(
+  @GetMapping(
   {
-    "/",
     "login",
     "login.html"
   })
@@ -41,7 +37,7 @@ public class ViewLoginController
   }
 
   @PostMapping("/login")
-  public String login(@RequestParam(name = "username") String aUsername, @RequestParam(name = "password") String aPassword, Model aModel)
+  public String doLogin(@RequestParam(name = "username") String aUsername, @RequestParam(name = "password") String aPassword, Model aModel)
   {
     try
     {
